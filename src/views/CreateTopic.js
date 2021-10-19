1641ec1afdc253548597bc256b638dbf36e01d73
salah856 <salah_elhossiny856@yahoo.com>
2021-10-19 15:04:06 +0200

    init the task

---
+++ src/views/CreateTopic.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+
+const CreateTopic = () => {
+
+    return (
+        <p>hello from topic create form </p>
+    )
+}
+
+
+export default CreateTopic; 
---
+++ src/views/index.js
@@ -0,0 +1,6 @@
+import ExternalApi from "./external-api";
+import Home from "./home";
+import Profile from "./profile";
+import CreateTopic from "./CreateTopic";
+
+export { ExternalApi, Home, Profile, CreateTopic };
